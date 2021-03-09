a8a2b071e1d0bac913e862644a92cae30a2ea347
SerranoMiguel <42155523+SerranoMiguel@users.noreply.github.com>
2021-03-09 03:25:17 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  fetch('https://fakestoreapi.com/products')
+  //Create a fetch request to this site: https://fakestoreapi.com/products
+  // This should return an array of JavaScript objects. Be careful - fetch is asynchronous
 });
