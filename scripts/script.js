2fb43c77689df235ca07f2ddb993dcd3d3fab5fe
SerranoMiguel <42155523+SerranoMiguel@users.noreply.github.com>
2021-03-09 06:08:12 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -7,5 +7,5 @@ window.addEventListener('DOMContentLoaded', () => {
   //Create a fetch request to this site: https://fakestoreapi.com/products
   // This should return an array of JavaScript objects. Be careful - fetch is asynchronous
   // add the array to local storage
-  localStorage.setItem(products, data);
+  localStorage.setItem('products', data);
 });
