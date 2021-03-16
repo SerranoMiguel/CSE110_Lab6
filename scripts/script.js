cfb61522a17b262ffcc8fcdc51d29e0243734121
Miguel Serrano <maserran@ucsd.edu>
2021-03-16 02:45:54 -0700

    local storage

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,7 @@ window.addEventListener('DOMContentLoaded', () => {
   //Create a fetch request to this site: https://fakestoreapi.com/products
   // This should return an array of JavaScript objects. Be careful - fetch is asynchronous
   // add the array to local storage
-  localStorage.setItem('products', JSON.stringify(response));
+  //localStorage.setItem('products', JSON.stringify(response));
   // says data is not defined
   // is data an issue?
 });
